ae63687f563e7418e7d76eb8e1a57853a642afe4
Spyridon Peppas <spiros3p@gmail.com>
2023-04-15 16:18:18 +0200

    fix lint error

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,17 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
-import { RetrieverComponent } from "./tools/retriever/retriever.component";
-import { MyAppsListComponent } from "./components/my-apps-list/my-apps-list.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { RetrieverComponent } from './tools/retriever/retriever.component';
+import { MyAppsListComponent } from './components/my-apps-list/my-apps-list.component';
 
 export const myRoutes = [
   {
-    path: "retriever",
+    path: 'xls-retriever',
     component: RetrieverComponent,
-    description:
-      "Get the important data you need. e.g., emails from an excel file",
+    description: 'Get the important data you need. e.g., emails from an excel file',
   },
 ];
 
-const routes: Routes = [
-  ...myRoutes,
-  { path: "", component: MyAppsListComponent },
-];
+const routes: Routes = [...myRoutes, { path: '', component: MyAppsListComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
