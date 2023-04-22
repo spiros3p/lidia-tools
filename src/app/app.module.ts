ef10e78cf50918562b06248f1384dc5ac7c60e32
Spyridon Peppas <spiros3p@gmail.com>
2023-04-22 22:37:14 +0200

    added column filter inputs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,20 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ToolsModule } from './tools/tools.module';
 import { ComponentsModule } from './components/components.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, NgbModule, ToolsModule, ComponentsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    ReactiveFormsModule,
+    FormsModule,
+    ToolsModule,
+    ComponentsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/tools/retriever/retriever.module.ts
+++ src/app/tools/retriever/retriever.module.ts
@@ -1,18 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RetrieverComponent } from './retriever.component';
-
-
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-  declarations: [
-    RetrieverComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    RetrieverComponent
-  ]
+  declarations: [RetrieverComponent],
+  imports: [CommonModule, FormsModule, FontAwesomeModule, ReactiveFormsModule],
+  exports: [RetrieverComponent],
 })
-export class RetrieverModule { }
+export class RetrieverModule {}
